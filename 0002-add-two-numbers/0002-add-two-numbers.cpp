3d946644058eba5ef7ae2bd9439275fdc09cbbd5
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-07-08 02:07:21 +0500

    Time: 27 ms (27.99%), Space: 75.9 MB (93.92%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -26,27 +26,6 @@ public:
         ptr->next=n;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // ListNode* ptr=new ListNode(0);
-        // ListNode* n=ptr;
-        // int carry=0;
-        // while(l1!=nullptr || l2!=nullptr)
-        // {
-        //     int num1=(l1!=nullptr) ? l1->val : 0;
-        //     int num2=(l2!=nullptr) ? l2->val : 0;
-        //     int sum=num1+num2+carry;
-        //     carry=sum/10;
-        //     n->next=new ListNode(sum%10);
-        //     n=n->next;
-        //     if(l1!=nullptr) 
-        //     l1=l1->next;
-        //     if(l2!=nullptr)
-        //     l2=l2->next;
-        // }
-        // if(carry>0)
-        // n->next=new ListNode(carry);
-        // ListNode* result=ptr->next;
-        // delete ptr;
-        // return result;
         ListNode* ptr1=l1;
         ListNode* ptr2=l2;
         ListNode* l=nullptr;
