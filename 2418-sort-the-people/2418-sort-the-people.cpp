e345da0ebce4f2d2f7a6ea36f04f0b3bdc47d182
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-07-23 02:56:17 +0500

    Time: 550 ms (5.06%), Space: 92 MB (5.01%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -19,3 +19,25 @@ public:
         return names;
     }
 };
+
+// With Recursion Time Complexity Error on 48th Test Case
+// class Solution {
+// public:
+//     vector<string> sortPeople(vector<string>& names, vector<int>& heights,int i=1,int size=0) {
+//         if(size==names.size())
+//         return names;
+//         if(i==names.size())
+//         return sortPeople(names,heights,size+1,size+1);
+//         if(heights[size]<heights[i])
+//         {
+//             int temp=heights[size];
+//             heights[size]=heights[i];
+//             heights[i]=temp;
+//             string tem=names[size];
+//             names[size]=names[i];
+//             names[i]=tem;
+//             return sortPeople(names,heights,i+1,size);
+//         }
+//         return sortPeople(names,heights,i+1,size);
+//     }
+// };
