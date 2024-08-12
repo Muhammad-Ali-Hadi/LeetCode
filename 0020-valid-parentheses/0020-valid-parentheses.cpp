ab34d215fec7553a243844db6269ffbcd63b0772
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-08-13 03:03:46 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     bool isValid(string s) 
     {
-        unordered_map<char,char> brackets={{'(',')'},{'[',']'},{'{','}'}};
+        // unordered_map<char,char> brackets={{'(',')'},{'[',']'},{'{','}'}};
+        // stack<char> st;
+        // for(auto& i : s)
+        // {
+        //     if(brackets.find(i)!=brackets.end())
+        //     {
+        //         st.push(i);
+        //     }
+        //     else
+        //     {
+        //         if(st.empty() || brackets[st.top()]!=i)
+        //         {
+        //             return false;
+        //         }
+        //         st.pop();
+        //     }
+        // }
+        // return st.empty();
+        if(s.size()%2!=0)
+        return false;
+
         stack<char> st;
-        for(auto& i : s)
+
+        for(int i=0;i<s.size();++i)
         {
-            if(brackets.find(i)!=brackets.end())
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
             {
-                st.push(i);
+                st.push(s[i]);
             }
             else
             {
-                if(st.empty() || brackets[st.top()]!=i)
-                {
-                    return false;
-                }
+                if(st.empty)
+                return false;
+                
+                if((st.top()!='{' && s[i]=='}') || (st.top()!='[' && s[i]==']') || (st.top()!='(' && s[i]==')'))
+                return false;
                 st.pop();
             }
         }
