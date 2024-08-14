b81b7a74d7622ac2631a2f77f1e492ae46e15d8a
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-08-15 04:48:05 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.empty())
+        return 1;
+
+        if(trust.size()==1)
+        return trust[0][1];
+
+        unordered_map<int,int> trustCount;
+        unordered_map<int,int> trustedBy;
+        for(int i=0;i<trust.size();++i)
+        {
+            trustCount[trust[i][0]]++;
+            trustedBy[trust[i][1]]++;
+        }
+
+        for(int i=0;i<trust.size();++i)
+        {
+            if(!trustCount[trust[i][1]] && trustedBy[trust[i][1]]==n-1)
+            return trust[i][1];
+        }
+
+        return -1;
+    }
+};
