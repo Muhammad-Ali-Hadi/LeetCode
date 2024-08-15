f03dbf87b4d06b6777557ed3047d96aa1e4f7e26
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-08-15 21:49:50 +0500

    Time: 3 ms (39.21%), Space: 8.4 MB (10.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    double myPow(double x,long long int n,double pow=1,long long int i=0) {
-        if(x==1 || n==0)
-        return 1;
-        if(x==-1)
-        return (n%2==0) ? 1 : -1;
+    double myPow(double x,long long int n) {
+        // if(x==1 || n==0)
+        // return 1;
+        // if(x==-1)
+        // return (n%2==0) ? 1 : -1;
 
-        // if(n>0)
+        // // if(n>0)
+        // // {
+        // //     if(i==n)
+        // //     return pow;
+        // //     return myPow(x,n,pow*x,i+1);
+        // // }
+        // // if(i==n)
+        // // return 1/pow;
+        // // return myPow(x,n,pow*x,i-1);
+
+        // if(n<0)
         // {
-        //     if(i==n)
-        //     return pow;
-        //     return myPow(x,n,pow*x,i+1);
+        //     x=1/x;
+        //     n*=-1;
         // }
+
         // if(i==n)
-        // return 1/pow;
-        // return myPow(x,n,pow*x,i-1);
+        // return pow;
+        // return myPow(x,n,x*pow,i+1);
+
+        if(n==0)
+        return 1;
 
         if(n<0)
         {
@@ -22,8 +35,11 @@ public:
             n*=-1;
         }
 
-        if(i==n)
-        return pow;
-        return myPow(x,n,x*pow,i+1);
+        double num=myPow(x,n/2);
+
+        if(n%2==0)
+        return num*num;
+        else
+        return num*num*x;
     }
 };
