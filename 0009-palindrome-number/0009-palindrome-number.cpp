e07bbca96f86d45bd84277c30d56f1436a6c3683
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-08-15 19:18:28 +0500

    Time: 14 ms (22.34%), Space: 12 MB (5.42%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool helper(string s,int n=0)
+    {
+        if(n==s.size())
+        return true;
+        if(s[n]!=s[s.size()-n-1])
+        {
+            return false;
+        }
+        return helper(s,n+1);
+    }
+
+    bool isPalindrome(int x) {
+        // string str=to_string(x);
+        
+        // int left=0;
+        // int right=str.size() - 1;
+        
+        // while(left<right) 
+        // {
+        //     if(str[left]!=str[right]) 
+        //     { 
+        //         return false; 
+        //     }
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+
+        // Solution with Indirect Recursion
+        if(x<0)
+        return false;
+        string str=to_string(x);
+        return helper(str);
+    }
+};
