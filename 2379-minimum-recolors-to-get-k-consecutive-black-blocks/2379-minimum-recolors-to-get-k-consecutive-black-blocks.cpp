2546d9c6bb884ad9df2c8662f143fcdbba0e4c7a
Muhammad-Ali-Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2024-08-17 22:05:39 +0500

    Time: 47 ms (15.66%), Space: 39.8 MB (6.13%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int minimumRecolors(string blocks, int k,int i=0,int j=0,int count=0) {
-        if(j==k)
-        return count;
+    int countWhites(string blocks,int k,int start)
+    {
+        if(k==0)
+        return 0;
 
-        int newCount=count+(blocks[i]=='W' ? 1 : 0);
-        int minRecolors=minimumRecolors(blocks,k,i+1,j+1,newCount);
+        return (blocks[start]=='W')+countWhites(blocks,k-1,start+1);
+    }
 
+    int minimumRecolors(string blocks, int k) {
+        int minRecolors=10e7;
 
-        if(i+k<blocks.size())
+        function<void(int)>slideWindow=[&](int start)
         {
-            minRecolors=min(minRecolors,minimumRecolors(blocks,k,i+1,0,0));
-        }
+            if(start+k>blocks.size())
+            return;
+
+            int recolors=countWhites(blocks,k,start);
+            minRecolors=min(minRecolors,recolors);
+
+            slideWindow(start+1);
+        };
+
+        slideWindow(0);
         return minRecolors;
     }
 };
