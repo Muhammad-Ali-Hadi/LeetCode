42384c7749e7f8c9c2f933015540b9c95ffc8b9d
Muhammad Ali Hadi <168474700+Muhammad-Ali-Hadi@users.noreply.github.com>
2026-01-16 05:31:18 +0500

    Time: 1359 ms (50.48%), Space: 267.2 MB (55.34%) - LeetHub

---
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -0,0 +1,36 @@
+#define speed ios_base::sync_with_stdio(false),cin.tie(nullptr)
+
+class Solution {
+public:
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        speed;
+
+        long long int ans=0,MOD=1e9+7;
+        unordered_set<int> set;
+        hFences.push_back(1);
+        hFences.push_back(m);
+
+        vFences.push_back(1);
+        vFences.push_back(n);
+
+        for(int i=0;i<hFences.size();++i)
+        {
+            for(int j=0;j<hFences.size();++j)
+            {
+                set.insert(abs(hFences[i]-hFences[j]));
+            }
+        }
+
+        for(int i=0;i<vFences.size();++i)
+        {
+            for(int j=0;j<vFences.size();++j)
+            {
+                if(i!=j && set.find(abs(vFences[i]-vFences[j]))!=set.end())
+                {
+                    ans=max(ans,(long long int)abs(vFences[i]-vFences[j]));
+                }
+            }
+        }
+        return !ans ? -1 : (ans*ans)%MOD;
+    }
+};
